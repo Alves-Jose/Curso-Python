847733adec260a9d0ac690ef4d44e5e51433a33f
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2023-05-04 16:08:18 -0300

    Update desafio69.py

--- desafios_python/desafio69.py
+++ desafios_python/desafio69.py
@@ -5,24 +5,28 @@
 c_age = 0
 c_mens = 0
 c_wom = 0
+gender = ' '
+question = ' '
 while True:
     print('-'*17)
     print('REGISTER A PERSON')
     print('-'*17)
 
     age = int(input('Age: '))
-    if age > 18:
+    if age >= 18:
         c_age += 1
         
-    gender = str(input('Gender: [M/F] ')).upper().strip()
+    gender = str(input('Gender: [M/F] ')).upper().strip()[0]
     while gender not in 'MF':
-        gender = str(input('Gender: [M/F] ')).upper().strip()
-        if gender == 'M':
-            c_mens = c_mens + 1
-        elif gender == 'F' and age < 20:
-            c_wom = c_wom + 1
+        gender = str(input('Gender: [M/F] ')).upper().strip()[0]
+    if gender == 'M':
+        c_mens = c_mens + 1
+    if gender == 'F' and age < 20:
+        c_wom = c_wom + 1
     print('-'*43)
-    question = str(input('Do you want to register someone else? [Y/N] ')).upper().strip()
+    question = str(input('Do you want to register someone else? [Y/N] ')).upper().strip()[0]
+    while question not in "YN":
+        question = str(input('Do you want to register someone else? [Y/N] ')).upper().strip()[0]
     if question == "N":
         print(f'{c_age} People are over 18 years old. {c_mens} Men were registered. {c_wom} Women are under 20 years old')
         break
