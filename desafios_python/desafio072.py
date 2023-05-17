6d9242441149bcfd4f01aa78e5a6a9ad6e2cf889
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2023-05-17 00:43:15 -0300

    Update desafio072.py

--- desafios_python/desafio072.py
+++ desafios_python/desafio072.py
@@ -1,1 +1,11 @@
-cont = 
+# Exercício Python 072: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+cont = ('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight','nine',
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty')
+
+while True:
+    n = int(input('Enter a number from 0 to 20: '))
+    if 0 <= n <= 20:
+        break
+    print('Try again.', end=' ')
+print(f'You typed the number {cont[n]}')
