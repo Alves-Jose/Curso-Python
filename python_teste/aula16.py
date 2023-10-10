8d2518544efbe47bab44c64667cecda2967cc952
Alves-Jose <josemarcos8133@gmail.com>
2023-10-10 12:15:54 -0300

    Update aula16.py

--- python_teste/aula16.py
+++ python_teste/aula16.py
@@ -1,17 +1,8 @@
-snack = ('Burguer', 'Juice', 'Pizza', 'Pudim', 'French fries')
+snack = ('Hamburguer', 'Juicy', 'Pizza', 'Pudding', 'French Fries')
 
-for cont in range(0, len(snack)):
-    print(f'I will eat {snack[cont]} in position {cont}')
-
-print('-'*10)
+# for cont in range(0, len(snack)):
+#     print(snack[cont])
 
 for food in snack:
-  print(f'I will eat {food}')
-
-
-print('-'*10)
-
-for pos, food in enumerate(snack):
-   print(f'I will eat {snack[cont]} in position {cont}')
-
-print('I eat so much')
+    print(f'I will eat {food}')
+print('I eat too much')
