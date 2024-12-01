870ade52a0e9e86201a5a5ebd714c77e2c008e08
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2024-12-01 17:06:15 -0300

    Update aula16.py

--- python_teste/aula16.py
+++ python_teste/aula16.py
@@ -1,4 +1,4 @@
-snack = ('Hamburguer', 'Juicy', 'Pizza', 'Pudding', 'French Fries')
+ snack = ('Hamburguer', 'Juicy', 'Pizza', 'Pudding', 'French Fries')
 
 # for cont in range(0, len(snack)):
 #     print(snack[cont])
