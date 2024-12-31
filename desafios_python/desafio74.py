cf0561f4d48bb4ea0c1095cb1ccdc50f53c99274
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2024-12-31 16:37:28 -0300

    Update desafio74.py

---
+++ desafios_python/desafio74.py
@@ -0,0 +1,10 @@
+from random import randint
+
+numeros = (randint (1, 5), randint (1, 5), randint (1, 5), randint (1, 5), randint (1, 5))
+print(f'Os números sorteados foram: ', end=' ')
+
+for n in numeros:
+    print(f'{n} ', end=' ')
+
+print(f'\nO maior valor sorteado foi {max(numeros)}')
+print(f'O menor valor sorteado foi {min(numeros)}')
