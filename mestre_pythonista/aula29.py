bcc6b9df12026dab88c539f91af2900076d7e3dc
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2025-03-11 13:36:00 -0300

    Create aula29.py

---
+++ mestre_pythonista/aula29.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+cartoes = ['R$50,00', 'R$250,00', 'R$120,00']
+
+nome = str(input("Insira seu nome: "))
+
+idade = int(input("Insira sua idade: "))
+
+dataregistro = datetime.now(), '%d/%m/%Y'
+
+print(dataregistro)
