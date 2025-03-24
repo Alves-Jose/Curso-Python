0639b1795a6500dd93b8c9ce5cf9cfb2c8080530
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2025-03-24 20:57:15 -0300

    Update aula47.2.py

--- mestre_pythonista/aula47.2.py
+++ mestre_pythonista/aula47.2.py
@@ -7,11 +7,38 @@ while True:
     direcao = str(input('Para qual direção devemos ir? F = Frente e T = Trás '))
 
     if direcao == 'F':
-        direcao = int(input('Quantos Pixels para frente? '))
-        direcao = t.forward(direcao)
-        
-    
-    else:
-        direcao = int(input('Quantos Pixels para trás? '))
-        direcao = t.backward(direcao)
+        distancia = int(input('Quantos Pixels para frente? '))
+        t.forward(distancia)
 
+        rotacao = str(input('Rotacionar para d:direita, e:esquerda n:não rotacionar - '))
+       
+        if rotacao == 'd':
+            angulo = int(input('Quantos pixels para a direita devemos rotacionar? '))
+            t.right(90)
+            t.forward(angulo)
+        
+        if rotacao == 'e':
+            angulo = int(input('Quantos pixels para a esquerda devemos rotacionar? '))
+            t.left(90)
+            t.forward(angulo)
+        
+    if direcao == 'T':
+        distancia = int(input('Quantos pixels para trás? '))
+        t.backward(distancia)
+
+        rotacao = str(input('Rotacionar para d:direita, e:esquerda n:não rotacionar - '))
+
+        if rotacao == 'd':
+            angulo = int(input('Quantos pixels para a direita devemos rotacionar? '))
+            t.right(90)
+            t.forward(angulo)
+        
+        if rotacao == 'e':
+            angulo = int(input('Quantos pixels para a esquerda devemos rotacionar? '))
+            t.left(90)
+            t.forward(angulo)
+        
+
+    continuar = input('Continuar andando? ')
+    if continuar not in ('Sim', 's'):
+        break
