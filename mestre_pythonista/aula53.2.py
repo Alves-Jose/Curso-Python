2ab29980d0404067eaabc6b08ac566b36ad2757c
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2025-04-09 21:57:05 -0300

    updaate

---
+++ mestre_pythonista/aula53.2.py
@@ -0,0 +1,4 @@
+def gerar_objeto_personalizado(cor, *, altura, formato):
+    print(cor, altura, formato)
+
+gerar_objeto_personalizado('Azul', altura= 1.70, formato= 'Triangular')
---
+++ mestre_pythonista/aula53.py
@@ -0,0 +1,5 @@
+def exibir_preco(nome_produto, preco):
+    print(f'{nome_produto} está no valor de: {preco}')
+
+exibir_preco('iPhone', 5000)
+#exibir_preco(nome_produto= 'iPhone', preco=5000)
