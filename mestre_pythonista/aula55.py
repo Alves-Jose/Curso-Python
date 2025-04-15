62d90bedfc3d6370d6931b1d2fe642d281d2932c
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2025-04-15 19:21:50 -0300

    commit

---
+++ mestre_pythonista/aula55.py
@@ -0,0 +1,7 @@
+def concatenar(**palavras):
+    frase = ''
+    for palavra in palavras.values():
+        frase += palavra + ' '
+    print(frase)
+
+concatenar(a='Nós', b='Somos', c='Pythonistas', d='Profissionais')
