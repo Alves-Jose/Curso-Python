a1f1cc41e9cfe4a1a031fe9dc72cfa7439a08d8a
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2025-05-28 15:42:47 -0300

    Update aula56.py

---
+++ mestre_pythonista/aula56.py
@@ -0,0 +1,13 @@
+from datetime import datetime   
+
+def depositar_dinheiro():
+    print('Depositando dinheiro')
+
+    def depositando_dolar():
+        print('Depositando dolares')
+
+    def depositando_reais():
+        print('Depositando reais')
+
+    depositando_dolar()
+    depositando_reais()C:\Users/Integral - 2/Documents/GitHub/Curso-Python/mestre_pythonista/aula56.py
