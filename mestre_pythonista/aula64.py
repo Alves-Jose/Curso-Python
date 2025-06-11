11dc2be2add4000fa4f0038de4e9b0ba89685bb8
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2025-06-11 17:26:46 -0300

    Update aula64.py

---
+++ mestre_pythonista/aula64.py
@@ -0,0 +1,16 @@
+#dicionarios
+
+'''
+Pessoa
+    nome
+    idade
+    altura
+'''
+
+pessoa = ['Carol', 18, 1.60, 'Mike', 50, 1.80]
+
+#dicionario(chave, valor)
+dicionario_pessoa = {'nome': 'Carol', 'idade': 18, 'altura': 1.60}
+pessoa_2 = dict(nome='Carol', idade=18, altura=1.60)
+print(dicionario_pessoa)
+print(pessoa_2)
