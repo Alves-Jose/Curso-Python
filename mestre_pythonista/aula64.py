ada131f56950bc7cacedd19f65f5cf2629c5ef41
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2025-06-14 23:27:37 -0300

    update

--- mestre_pythonista/aula64.py
+++ mestre_pythonista/aula64.py
@@ -11,6 +11,13 @@ pessoa = ['Carol', 18, 1.60, 'Mike', 50, 1.80]
 
 #dicionario(chave, valor)
 dicionario_pessoa = {'nome': 'Carol', 'idade': 18, 'altura': 1.60}
-pessoa_2 = dict(nome='Carol', idade=18, altura=1.60)
-print(dicionario_pessoa)
-print(pessoa_2)
+#pessoa_2 = dict(nome='Carol', idade=18, altura=1.60)
+
+print(dicionario_pessoa.keys())
+print(dicionario_pessoa.values())
+print(dicionario_pessoa.items())
+
+#iterar sobre um dicionario
+
+for item in dicionario_pessoa.items():
+    print(item[1])
