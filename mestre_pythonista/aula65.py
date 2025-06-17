ff187ad153467e2649fcb00026f969ae65b63c3f
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2025-06-16 23:18:25 -0300

    updates

--- mestre_pythonista/aula65.py
+++ mestre_pythonista/aula65.py
@@ -1,2 +1,19 @@
 #Tuplas
 
+site1 = 'youtube.com'
+site2 = 'facebook.com'
+site3 = 'instagram.com'
+
+#ou 
+
+valor1 = 22
+valor2 = 43
+valor3 = 65
+
+sites = ('youtube.com', 'facebook.com','instagram.com' )
+valores = (23, 43, 65)
+
+#União de tuplas
+
+dados_do_sistema = sites + valores
+print(dados_do_sistema)
---
+++ mestre_pythonista/aula67.py
@@ -0,0 +1,7 @@
+
+for numero in range(10, 30, 2):
+    print(numero)
+
+#criar listas mais rapidamente
+resultado = list(range(0, 201, 10))
+print(resultado)
