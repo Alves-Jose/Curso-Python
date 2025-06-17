509c364304e1ed7d3d7f25b6cd4cdec6b0780138
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2025-06-17 12:20:54 -0300

    Create aula72.py

---
+++ mestre_pythonista/aula72.py
@@ -0,0 +1,11 @@
+numeros = [2, 2, 5, 8]
+frutas = {'Maça', 'Uva', 'Banana', 'Maça', 'Morango'}
+
+set_numeros = set(numeros)
+set_frutas = set(frutas)
+
+print(set_numeros)
+print(set_frutas)
+
+set_numeros.add(10)
+print(set_numeros)
